2c0e375e3b89f7d865535e67c1c31af56bb95991
Nan Jiang <johnny.nan.jiang@gmail.com>
2019-06-26 09:42:40 +0800

    Add tests for function pointer, vector, volatile

--- test/practice/book.cpp
+++ test/practice/book.cpp
@@ -8,3 +8,39 @@
 
 using namespace std;
 
+void showReview(const string & review) {
+    cout << review << endl;
+}
+
+void (*showReviewPtr)(const string & review);
+
+// chapter 7 @ page 329 in book
+TEST(cpp_primer_plus_5th, function_pointer) {
+    vector<string> reviews = {"a", "b", "c"};
+    vector<string>::iterator iterator;
+
+    showReviewPtr = showReview;
+
+    for_each(reviews.begin(), reviews.end(), showReviewPtr);
+}
+
+// chapter 16 @ page 885 in book
+TEST(cpp_primer_plus_5th, vector) {
+    vector<string> reviews {"a", "b", "c"};
+    vector<string>::iterator iterator;
+
+    for_each(reviews.begin(), reviews.end(), showReview);
+}
+
+// https://liam.page/2018/01/18/volatile-in-C-and-Cpp/
+TEST(cpp, volatile) {
+    volatile int *p = new int(0);
+    int a, b;
+    a = *p;
+    b = *p;
+
+    ASSERT_EQ(0, a);
+    ASSERT_EQ(0, b);
+
+    delete p;
+}
