0652cb4ebd4adfeccd0f2627d885dba04493b11f
Nan Jiang <johnny.nan.jiang@gmail.com>
2019-06-27 12:45:22 +0800

    Implement Leetcode Median of Two Sorted Arrays @ https://leetcode.com/problems/median-of-two-sorted-arrays/

--- src/leetcode/median_of_two_sorted_arrays/solution.h
+++ src/leetcode/median_of_two_sorted_arrays/solution.h
@@ -9,12 +9,23 @@ using namespace std;
 
 class Solution {
 public:
-    static double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        return -1.0;
+    static double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+        vector<int> merged;
+
+        set_union(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), std::back_inserter(merged));
+
+        size_t size = merged.size();
+
+        size_t midIndex = size / 2;
+
+        if (size == 0) return -1;
+
+        return ((size % 2) == 0) ? (merged[midIndex - 1] + merged[midIndex]) / 2.0 : merged[midIndex];
     }
 
-    static vector<int> mergeSortedArrays(vector<int>& nums1, vector<int>& nums2, vector<int>& result) {
-        set_union(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), result.begin());
+    static vector<int> mergeSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+        vector<int> result;
+        set_union(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), std::back_inserter(result));
         return result;
     }
 };
--- test/leetcode/median_of_two_sorted_arrays/solution_tests.cpp
+++ test/leetcode/median_of_two_sorted_arrays/solution_tests.cpp
@@ -10,10 +10,8 @@
 using namespace std;
 
 TEST(leetcode, merge_two_sorted_arrays) {
-    vector<int> a, b, result;
-
-    a = {0}, b = {0, 1, 2}, result = {0, 1, 2};
-    Solution::mergeSortedArrays(a, b, result);
+    vector<int> a{0, 2}, b{0, 1, 2, 3}, expectedResult{0, 1, 2, 3};
+    ASSERT_EQ(expectedResult, Solution::mergeSortedArrays(a, b));
 }
 
 TEST(leetcode, median_of_two_sorted_arrays) {
