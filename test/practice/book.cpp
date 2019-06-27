58385d5071d81ecc392d0be6ae59f678510d8d56
Nan Jiang <johnny.nan.jiang@gmail.com>
2019-06-27 09:29:37 +0800

    Add test for sorting vector

--- test/practice/book.cpp
+++ test/practice/book.cpp
@@ -8,13 +8,13 @@
 
 using namespace std;
 
+// C++ Primer Plus 5th Edition, chapter 7 @ page 329 in book
 void showReview(const string & review) {
     cout << review << endl;
 }
 
 void (*showReviewPtr)(const string & review);
 
-// chapter 7 @ page 329 in book
 TEST(cpp_primer_plus_5th, function_pointer) {
     vector<string> reviews = {"a", "b", "c"};
     vector<string>::iterator iterator;
@@ -24,11 +24,9 @@ TEST(cpp_primer_plus_5th, function_pointer) {
     for_each(reviews.begin(), reviews.end(), showReviewPtr);
 }
 
-// chapter 16 @ page 885 in book
-TEST(cpp_primer_plus_5th, vector) {
+// C++ Primer Plus 5th Edition, chapter 16 @ page 885 in book
+TEST(cpp_primer_plus_5th, vector_iteration) {
     vector<string> reviews {"a", "b", "c"};
-    vector<string>::iterator iterator;
-
     for_each(reviews.begin(), reviews.end(), showReview);
 }
 
@@ -43,4 +41,31 @@ TEST(cpp, volatile) {
     ASSERT_EQ(0, b);
 
     delete p;
+}
+
+// C++ Primer Plus 5th Edition, chapter 16 @ page 923 in book
+bool sortAlphabetically(string & a, string & b) {
+    return a < b;
+}
+
+// TODO: to be implemented properly
+template<typename T>
+string toString(T & v) {
+    string str = std::to_string(std::forward<T>(v));
+    cout << str << endl;
+    return str;
+}
+
+TEST(cpp_primer_plus_5th, vector_sort) {
+    vector<string> reviews {"d", "a", "c", "b"};
+
+    for(string s : reviews) {
+        cout << s << endl;
+    }
+
+    sort(reviews.begin(), reviews.end(), sortAlphabetically);
+
+    for(string s : reviews) {
+        cout << s << endl;
+    }
 }
