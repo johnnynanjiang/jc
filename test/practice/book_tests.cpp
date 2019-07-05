5fa63e7763e40824dfc67b71aa5ab74b905ea7bd
Nan Jiang <johnny.nan.jiang@gmail.com>
2019-07-05 12:47:47 +0800

    Add test for constructing 2D array with dynamic row and column size

--- test/practice/book_tests.cpp
+++ test/practice/book_tests.cpp
@@ -5,15 +5,16 @@
 #include "gtest/gtest.h"
 
 #include <string>
+#include <thread>
 
 using namespace std;
 
 // C++ Primer Plus 5th Edition, chapter 7 @ page 329 in book
-void showReview(const string & review) {
+void showReview(const string &review) {
     cout << review << endl;
 }
 
-void (*showReviewPtr)(const string & review);
+void (*showReviewPtr)(const string &review);
 
 TEST(cpp_primer_plus_5th, function_pointer) {
     vector<string> reviews = {"a", "b", "c"};
@@ -26,7 +27,7 @@ TEST(cpp_primer_plus_5th, function_pointer) {
 
 // C++ Primer Plus 5th Edition, chapter 16 @ page 885 in book
 TEST(cpp_primer_plus_5th, vector_iteration) {
-    vector<string> reviews {"a", "b", "c"};
+    vector<string> reviews{"a", "b", "c"};
     for_each(reviews.begin(), reviews.end(), showReview);
 }
 
@@ -44,28 +45,89 @@ TEST(cpp, volatile) {
 }
 
 // C++ Primer Plus 5th Edition, chapter 16 @ page 923 in book
-bool sortAlphabetically(string & a, string & b) {
+bool sortAlphabetically(string &a, string &b) {
     return a < b;
 }
 
 // TODO: to be implemented properly
 template<typename T>
-string toString(T & v) {
+string toString(T &v) {
     string str = std::to_string(std::forward<T>(v));
     cout << str << endl;
     return str;
 }
 
 TEST(cpp_primer_plus_5th, vector_sort) {
-    vector<string> reviews {"d", "a", "c", "b"};
+    vector<string> reviews{"d", "a", "c", "b"};
 
-    for(string s : reviews) {
+    for (string s : reviews) {
         cout << s << endl;
     }
 
     sort(reviews.begin(), reviews.end(), sortAlphabetically);
 
-    for(string s : reviews) {
+    for (string s : reviews) {
         cout << s << endl;
     }
+}
+
+// IO library C++ Primer 5th @ page 400 in PDF
+
+TEST(cpp_primer_5th, io_cin) {
+
+}
+
+// C++ Concurrency In Action
+
+void doSomething() {
+    sleep(10);
+    cout << "do something" << endl;
+}
+
+TEST(cpp_cocurrency_in_action, thread) {
+    std::thread t(doSomething);
+    t.join();
+}
+
+// 数据结构、算法与应用 C++语言描述
+
+template<class T>
+void make2DArray(T ** &array, size_t rowSize, size_t columnSize) {
+    try {
+        array = new T*[rowSize];
+
+        for (size_t i = 0; i < rowSize; ++i) {
+            array[i] = new T[columnSize] {};
+        }
+    } catch (bad_alloc) {
+        cout << "exception: bad_alloc" << endl;
+    }
+}
+
+template<class T>
+void print2DArray(T ** const &array, size_t rowSize, size_t columnSize) {
+    for (size_t i = 0; i < rowSize; ++i) {
+        for (size_t j = 0; j < columnSize; ++j) {
+            cout << array[i][j] << ", ";
+        }
+        cout << endl;
+    }
+}
+
+template<class T>
+void delete2DArray(T ** &array, size_t rowSize) {
+    for (size_t i = 0; i < rowSize; ++i) {
+        delete []array[i];
+    }
+
+    delete []array;
+}
+
+TEST(data_structure_algorithm_application_cpp, new_2d_array) {
+    int ** array;
+    size_t rowSize = 10, columnSize = 10;
+
+    make2DArray<int>(array, rowSize, columnSize);
+    print2DArray<int>(array, rowSize, columnSize);
+    delete2DArray<int>(array, rowSize);
 }
