f80b6b382adb7ebf8ae6b5b5f8c175cf4b56969b
Nan Jiang <johnny.nan.jiang@gmail.com>
2019-07-18 16:57:10 +0800

    Submitted https://leetcode.com/problems/print-binary-tree/ @ https://leetcode.com/submissions/detail/244319266/
    Runtime: 4 ms, faster than 77.99% of C++ online submissions for Print Binary Tree.
    Memory Usage: 9.7 MB, less than 33.33% of C++ online submissions for Print Binary Tree.

--- test/leetcode/search/binary_search_tree_tests.cpp
+++ test/leetcode/search/binary_search_tree_tests.cpp
@@ -80,24 +80,20 @@ void insert(TreeNode *&rootNodePtr, int value) {
     }
 }
 
-uint32_t height(const TreeNode *&rootNodePtr) {
+uint32_t height(TreeNode *&rootNodePtr) {
     if (rootNodePtr == nullptr) return 0;
-    return 1 + std::max(height(const_cast<const TreeNode *&>(rootNodePtr->left)),
-                        height(const_cast<const TreeNode *&>(rootNodePtr->right)));
+    return 1 + std::max(height(rootNodePtr->left),
+                        height(rootNodePtr->right));
 }
 
-int height_balance(const TreeNode *&rootNodePtr) {
+int height_balance(TreeNode *&rootNodePtr) {
     if (rootNodePtr == nullptr) return 0;
-    return height(const_cast<const TreeNode *&>(rootNodePtr->left)) -
-           height(const_cast<const TreeNode *&>(rootNodePtr->right));
+    return height(rootNodePtr->left) -
+           height(rootNodePtr->right);
 }
 
 uint32_t max_capacity(uint32_t height) {
     return std::pow(2, height) - 1;
-}
-
-void balance_tree(TreeNode *&rootNodePtr) {
-
 }
 
 TreeNode *make_tree(const vector<int> &vector) {
@@ -126,7 +122,7 @@ std::string to_string(std::vector<const TreeNode *> nodeList) {
     std::string output{};
 
     for (auto node : nodeList) {
-        output += (node == nullptr ? "*" : to_string(node->val)) + ",";
+        output += (node == nullptr ? "*" : std::to_string(node->val)) + ",";
     }
 
     return output;
@@ -148,7 +144,7 @@ std::string to_string(const std::vector<std::vector<string>> &matrix) {
     return output;
 }
 
-std::vector<const TreeNode *> to_list(const TreeNode *rootNodePtr) {
+std::vector<const TreeNode *> to_list(TreeNode *rootNodePtr) {
     uint32_t treeHeight = height(rootNodePtr);
     uint32_t maxCapacity = max_capacity(treeHeight - 1);
     vector<const TreeNode *> nodeList;
@@ -177,7 +173,7 @@ uint32_t width(uint32_t height) {
     uint32_t i = 0;
     uint32_t width = 0;
 
-    while (i <= height) {
+    while (i < height) {
         width += std::pow(2, i);
         ++i;
     }
@@ -185,22 +181,23 @@ uint32_t width(uint32_t height) {
     return width;
 }
 
-std::vector<std::vector<string>> draw_tree(const TreeNode *&rootNodePtr) {
+std::vector<std::vector<string>> draw_tree(TreeNode *&rootNodePtr) {
     uint32_t matrixHeight = height(rootNodePtr);
     uint32_t matrixWidth = width(matrixHeight);
     std::vector<const TreeNode *> nodeList = to_list(rootNodePtr);
-    std::vector<std::vector<string>> matrix(matrixHeight, std::vector<string>(matrixWidth, ""));
+    std::vector<std::vector<string>> matrix(matrixHeight, std::vector<string>(matrixWidth, " "));
 
-    uint32_t startIndex = 0;
+    uint32_t currentIndexInNodeList = 0;
     for (uint32_t i = 0; i < matrixHeight; ++i) {
         uint32_t numberOfNodes = std::pow(2, i);
-        uint32_t startPosition = matrixWidth / (2 * numberOfNodes);
+        uint32_t currentPositionInMatrix = matrixWidth / (2 * numberOfNodes);
+        uint32_t interval = (matrixWidth / (2 * std::pow(2, i - 1))) + 1;
 
         for (uint32_t j = 0; j < numberOfNodes; ++j) {
-            matrix[i][startPosition] = (nodeList[startIndex] == nullptr ?
-                                        "" : std::to_string(nodeList[startIndex]->val));
-            ++startIndex;
-            startPosition += 0;
+            matrix[i][currentPositionInMatrix] = (nodeList[currentIndexInNodeList] == nullptr ?
+                                                  " " : std::to_string(nodeList[currentIndexInNodeList]->val));
+            ++currentIndexInNodeList;
+            currentPositionInMatrix += interval;
         }
     }
 
@@ -225,56 +222,81 @@ TEST(TreeNode, make_tree) {
 }
 
 TEST(TreeNode, tree_height) {
-    const TreeNode *rootNodePtr = make_tree(fixture_vector);
+    TreeNode *rootNodePtr = make_tree(fixture_vector);
 
     ASSERT_EQ(4, height(rootNodePtr));
-    ASSERT_EQ(3, height(const_cast<const TreeNode *&>(rootNodePtr->right)));
-    ASSERT_EQ(2, height(const_cast<const TreeNode *&>(rootNodePtr->right->right)));
-    ASSERT_EQ(1, height(const_cast<const TreeNode *&>(rootNodePtr->right->right->right)));
-    ASSERT_EQ(0, height(const_cast<const TreeNode *&>(rootNodePtr->right->right->right->right)));
+    ASSERT_EQ(3, height(rootNodePtr->right));
+    ASSERT_EQ(2, height(rootNodePtr->right->right));
+    ASSERT_EQ(1, height(rootNodePtr->right->right->right));
+    ASSERT_EQ(0, height(rootNodePtr->right->right->right->right));
 }
 
 TEST(TreeNode, tree_height_balance) {
-    const TreeNode *rootNodePtr = make_tree(fixture_vector);
+    TreeNode *rootNodePtr = make_tree(fixture_vector);
 
     ASSERT_EQ(-3, height_balance(rootNodePtr));
-    ASSERT_EQ(-2, height_balance(const_cast<const TreeNode *&>(rootNodePtr->right)));
-    ASSERT_EQ(-1, height_balance(const_cast<const TreeNode *&>(rootNodePtr->right->right)));
-    ASSERT_EQ(0, height_balance(const_cast<const TreeNode *&>(rootNodePtr->right->right->right)));
+    ASSERT_EQ(-2, height_balance(rootNodePtr->right));
+    ASSERT_EQ(-1, height_balance(rootNodePtr->right->right));
+    ASSERT_EQ(0, height_balance(rootNodePtr->right->right->right));
 }
 
 TEST(TreeNode, tree_height_balance_reverse) {
-    const TreeNode *rootNodePtr = make_tree(fixture_vector_reverse);
+    TreeNode *rootNodePtr = make_tree(fixture_vector_reverse);
 
     ASSERT_EQ(3, height_balance(rootNodePtr));
-    ASSERT_EQ(2, height_balance(const_cast<const TreeNode *&>(rootNodePtr->left)));
-    ASSERT_EQ(1, height_balance(const_cast<const TreeNode *&>(rootNodePtr->left->left)));
-    ASSERT_EQ(0, height_balance(const_cast<const TreeNode *&>(rootNodePtr->left->left->left)));
+    ASSERT_EQ(2, height_balance(rootNodePtr->left));
+    ASSERT_EQ(1, height_balance(rootNodePtr->left->left));
+    ASSERT_EQ(0, height_balance(rootNodePtr->left->left->left));
 }
 
 TEST(TreeNode, tree_to_queue) {
-    const TreeNode *rootNodePtr = make_tree(fixture_vector);
+    TreeNode *rootNodePtr = make_tree(fixture_vector);
 
     vector<const TreeNode *> nodeList = to_list(rootNodePtr);
 
     ASSERT_EQ("0,*,1,*,*,*,2,*,*,*,*,*,*,*,3,", to_string(nodeList));
 }
 
+TEST(TreeNode, matrix_width) {
+    ASSERT_EQ(1, width(1));
+    ASSERT_EQ(3, width(2));
+    ASSERT_EQ(7, width(3));
+    ASSERT_EQ(15, width(4));
+}
+
 TEST(TreeNode, draw_full_tree) {
     const vector<int> full_tree_vector = {10, 5, 50, 1, 6, 30, 60};
-    const TreeNode *rootNodePtr = make_tree(full_tree_vector);
+    TreeNode *rootNodePtr = make_tree(full_tree_vector);
+
+    vector<const TreeNode *> nodeList = to_list(rootNodePtr);
+
+    ASSERT_EQ("10,5,50,1,6,30,60,", to_string(nodeList));
 
     auto matrix = draw_tree(rootNodePtr);
 
-    ASSERT_EQ("", to_string(matrix));
+    cout << to_string(matrix) << endl;
 }
 
 TEST(TreeNode, draw_ugly_tree) {
-    const TreeNode *rootNodePtr = make_tree(fixture_vector);
+    TreeNode *rootNodePtr = make_tree(fixture_vector);
 
-    draw_tree(rootNodePtr);
+    vector<const TreeNode *> nodeList = to_list(rootNodePtr);
+
+    ASSERT_EQ("0,*,1,*,*,*,2,*,*,*,*,*,*,*,3,", to_string(nodeList));
+
+    auto matrix = draw_tree(rootNodePtr);
+
+    cout << to_string(matrix) << endl;
 }
 
-TEST(search, binary_search_tree) {
+TEST(TreeNode, draw_simple_tree) {
+    TreeNode *rootNodePtr = make_tree({1, 2});
 
+    vector<const TreeNode *> nodeList = to_list(rootNodePtr);
+
+    ASSERT_EQ("1,*,2,", to_string(nodeList));
+
+    auto matrix = draw_tree(rootNodePtr);
+
+    cout << to_string(matrix) << endl;
 }
